03cfabc9b3fe5bcae6668cb599d6bf21355a8fcc
Jorgen Carlsen <jscarlsen@gmail.com>
2020-10-17 17:17:24 -0700

    Implement scripts for seeding postgres

---
+++ database/postgresql/generateUsers.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+const faker = require('faker');
+
+const createUsers = () => {
+  const writer = csvWriter();
+  writer.pipe(fs.createWriteStream('./csv/users.csv'));
+  for (let i = 0; i < 8000000; i++) {
+    if (i % 800000 === 0) {
+      console.log(`${(i / 8000000) * 100}% users complete`);
+    }
+    writer.write({
+      username: faker.internet.userName() + (Math.floor(Math.random() * 50)),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      phone: faker.phone.phoneNumberFormat(1),
+      email: faker.internet.email(),
+    });
+  }
+
+  writer.end();
+  console.log('Users done.');
+};
+
+createUsers();
--- database/postgresql/schema.sql
+++ database/postgresql/schema.sql
@@ -18,16 +18,16 @@ CREATE TABLE IF NOT EXISTS dishes (
   id SERIAL PRIMARY KEY,
   restaurant_id INT NOT NULL REFERENCES restaurants(id),
   name VARCHAR(50),
-  description VARCHAR(255)
+  description VARCHAR(500)
 );
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
+  username VARCHAR(50) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   phone VARCHAR(15),
-  email VARCHAR(50) UNIQUE NOT NULL
+  email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS reviews(
@@ -35,6 +35,14 @@ CREATE TABLE IF NOT EXISTS reviews(
   dish_id INT NOT NULL REFERENCES dishes(id),
   user_id INT NOT NULL REFERENCES users(id),
   review_text VARCHAR(1000) NOT NULL,
-  dined_on DATE,
+  dined_on VARCHAR(100),
   rating FLOAT
 );
+
+\COPY restaurants (name, cuisine, phone, street, city, state, zip_code) FROM '/Users/jorgencarlsen/Hack-Reactor/sdc/popular-dishes-service/database/postgresql/csv/restaurants.csv' CSV HEADER;
+
+\COPY users (username, first_name, last_name, phone, email) FROM '/Users/jorgencarlsen/Hack-Reactor/sdc/popular-dishes-service/database/postgresql/csv/users.csv' CSV HEADER;
+
+\COPY dishes (restaurant_id, name, description) FROM '/Users/jorgencarlsen/Hack-Reactor/sdc/popular-dishes-service/database/postgresql/csv/dishes.csv' CSV HEADER;
+
+\COPY reviews (dish_id, user_id, review_text, dined_on, rating) FROM '/Users/jorgencarlsen/Hack-Reactor/sdc/popular-dishes-service/database/postgresql/csv/reviews.csv' CSV HEADER;
